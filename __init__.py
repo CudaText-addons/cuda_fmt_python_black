834c5ec11897b6bb7be4bc9efbb9e7bdbf77c324
Alexey-t <support@uvviewsoft.com>
2019-07-14 01:55:42 +0300

    refac

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ PY_OK = sys.version_info[:2] >= (3, 6)
 if PY_OK:
     sys.path.append(os.path.dirname(__file__))
     from . import black
+    from .black import TargetVersion as Ver
 
 
 def get_mode():
@@ -22,60 +23,63 @@ def get_mode():
     norm = d.get('string_normalization', True)
     v = d.get('target')
 
-    if v=='py27':
-        ver = {
-            black.TargetVersion.PY27,
-            black.TargetVersion.PY33,
-            black.TargetVersion.PY34,
-            black.TargetVersion.PY35,
-            black.TargetVersion.PY36,
-            black.TargetVersion.PY37,
-            black.TargetVersion.PY38,
+    ver27 = {
+            Ver.PY27,
+            Ver.PY33,
+            Ver.PY34,
+            Ver.PY35,
+            Ver.PY36,
+            Ver.PY37,
+            Ver.PY38,
             }
-    elif v=='py33':
-        ver = {
-            black.TargetVersion.PY33,
-            black.TargetVersion.PY34,
-            black.TargetVersion.PY35,
-            black.TargetVersion.PY36,
-            black.TargetVersion.PY37,
-            black.TargetVersion.PY38,
+    ver33 = {
+            Ver.PY33,
+            Ver.PY34,
+            Ver.PY35,
+            Ver.PY36,
+            Ver.PY37,
+            Ver.PY38,
             }
-    elif v=='py34':
-        ver = {
-            black.TargetVersion.PY34,
-            black.TargetVersion.PY35,
-            black.TargetVersion.PY36,
-            black.TargetVersion.PY37,
-            black.TargetVersion.PY38,
+    ver34 = {
+            Ver.PY34,
+            Ver.PY35,
+            Ver.PY36,
+            Ver.PY37,
+            Ver.PY38,
             }
-    elif v=='py35':
-        ver = {
-            black.TargetVersion.PY35,
-            black.TargetVersion.PY36,
-            black.TargetVersion.PY37,
-            black.TargetVersion.PY38,
+    ver35 = {
+            Ver.PY35,
+            Ver.PY36,
+            Ver.PY37,
+            Ver.PY38,
             }
-    elif v=='py36':
-        ver = {
-            black.TargetVersion.PY36,
-            black.TargetVersion.PY37,
-            black.TargetVersion.PY38,
+    ver36 = {
+            Ver.PY36,
+            Ver.PY37,
+            Ver.PY38,
             }
-    elif v=='py37':
-        ver = {
-            black.TargetVersion.PY37,
-            black.TargetVersion.PY38,
+    ver37 = {
+            Ver.PY37,
+            Ver.PY38,
             }
-    else:
-        ver = {
-            black.TargetVersion.PY38,
+    ver38 = {
+            Ver.PY38,
             }
 
+    ver_map = {
+        'py27': ver27,
+        'py33': ver33,
+        'py34': ver34,
+        'py35': ver35,
+        'py36': ver36,
+        'py37': ver37,
+        'py38': ver38,
+    }
+
     return black.FileMode(
-        target_versions=ver, 
-        line_length=line_len,
-        string_normalization=norm
+        target_versions = ver_map.get(v, ver38),
+        line_length = line_len,
+        string_normalization = norm
         )
 
 
