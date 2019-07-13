ad7441301fbffad76ab34cc9b55cdf6e0379e8ed
Alexey-t <support@uvviewsoft.com>
2019-07-13 23:29:42 +0300

    check py version

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(__file__))
-from . import black
+PY_OK = sys.version_info[:2] >= (3, 6)
+if PY_OK:
+    sys.path.append(os.path.dirname(__file__))
+    from . import black
 
 def do_format(text):
 
+    if not PY_OK: return
     mode = black.FileMode()
     text = black.format_str(text, mode=mode)
     return text
